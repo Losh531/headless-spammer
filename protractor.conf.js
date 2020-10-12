8ac42355c281baa8ee7790f07b91acea877d7251
Losh531 <65097694+Losh531@users.noreply.github.com>
2020-10-12 06:26:27 -0400

    Create protractor.conf.js

---
+++ protractor.conf.js
@@ -0,0 +1,9 @@
+ "selenium": {
+    "start_process": true,
+    "server_path": "./bin/selenium-server-standalone-3.141.59.jar",
+    "log_path": "",
+    "port": 4444,
+    "cli_args": {
+      "webdriver.chrome.driver": "node_modules/chromedriver/lib/chromedriver/chromedriver.exe"
+    }
+  }
